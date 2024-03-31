1d366b5fce50ec122fe4a7cb813620d0bd905534
LTS-FFXIV <127939494+LTS-FFXIV@users.noreply.github.com>
2024-03-31 07:53:27 -0500

    more reaper fixes

--- BasicRotations/Melee/RPR_Default.cs
+++ BasicRotations/Melee/RPR_Default.cs
@@ -10,7 +10,7 @@ public sealed class RPR_Default : ReaperRotation
     protected override IAction? CountDownAction(float remainTime)
     {
         if (remainTime < HarpePvE.Info.CastTime + CountDownAhead
-            && HarpePvE.CanUse(out var act), ) return act;
+            && HarpePvE.CanUse(out var act)) return act;
 
         if (SoulsowPvE.CanUse(out act)) return act;
 
@@ -74,14 +74,14 @@ public sealed class RPR_Default : ReaperRotation
 
         if (HasSoulReaver)
         {
-            if (GuillotinePvE.CanUse(out act), skipClippingCheck: true) return true;
+            if (GuillotinePvE.CanUse(out act, skipClippingCheck: true)) return true;
             if (Player.HasStatus(true, StatusID.EnhancedGibbet))
             {
-                if (GibbetPvE.CanUse(out act), skipClippingCheck: true, skipComboCheck: true) return true;
+                if (GibbetPvE.CanUse(out act, skipClippingCheck: true, skipComboCheck: true)) return true;
             }
             else
             {
-                if (GallowsPvE.CanUse(out act), skipClippingCheck: true, skipComboCheck: true) return true;
+                if (GallowsPvE.CanUse(out act, skipClippingCheck: true, skipComboCheck: true)) return true;
             }
         }
 
