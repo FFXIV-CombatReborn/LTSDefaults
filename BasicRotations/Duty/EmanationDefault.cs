ca711724146caf7dc5b97977eb0c65df08792a45
Tykku <matpmolle@gmail.com>
2024-05-13 06:14:51 -0400

    Testmination 1

--- BasicRotations/Duty/EmanationDefault.cs
+++ BasicRotations/Duty/EmanationDefault.cs
@@ -1,6 +1,4 @@
-﻿using RotationSolver.Basic.Rotations.Duties;
-
-namespace DefaultRotations.Duty;
+﻿namespace DefaultRotations.Duty;
 
 [Rotation("Emanation Default", CombatType.PvE)]
 [DutyTerritory(263, 264)]
@@ -8,8 +6,16 @@ internal class EmanationDefault : DutyRotation
 {
     protected override bool EmergencyAbility(IAction nextGCD, out IAction? act)
     {
-        if (VrilPvE.CanUse(out act)) return true; // Normal
-        if (VrilPvE_9345.CanUse(out act)) return true; // Extreme
-        return base.EmergencyAbility(nextGCD, out act);
+        // 8521 8522 8523
+        bool Lol1 = HostileTarget?.CastActionId == 8521
+        bool Lol2 = HostileTarget?.CastActionId == 8522
+        bool Lol3 = HostileTarget?.CastActionId == 8523
+
+        if (Lol1 && Lol2 && Lol3)
+        {
+            if (VrilPvE.CanUse(out act)) return true; // Normal
+            if (VrilPvE_9345.CanUse(out act)) return true; // Extreme
+            return base.EmergencyAbility(nextGCD, out act);
+        }
     }
 }
